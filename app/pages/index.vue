fccef339c7c076bbe36d64b7f6f8f3c0830ce178
ak-yoshimatsu <codetrain.akihiro.yoshimatsu@gmail.com>
2025-07-26 10:55:56 +0900

    tailwind + shadcn?

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <h1>インデックスページ</h1>
+    <h1 class="bg-sky-500">インデックスページ</h1>
 
     <div>
       <p>コンテンツが入る</p>
+      <Button>Click me</Button>
     </div>
   </div>
 </template>
